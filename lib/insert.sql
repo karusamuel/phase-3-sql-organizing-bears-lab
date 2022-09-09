f63f46e1599f8f9286d88576b8ad3c1fd90044cc
Karu Samuel <karusamuelngugi@gmail.com>
2022-09-09 14:08:03 +0300

    Complete Assignment

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Mr. Chocolate",23,"M","Green","Angry",0);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Rowdy",23,"M","Green","Angry",1);
+INSERT INTO bears(name,age,sex,color,temparament,alive) VALUES("Tabitha",23,"F","Green","Angry",0);
+INSERT INTO bears(name,age,sex,color,temparament,alive) VALUES("Sergeant Brown",23,"F","Green","Angry",1);
+INSERT INTO bears(name,age,sex,color,temparament,alive) VALUES("Melissa",23,"F","Green","Angry",1);
+INSERT INTO bears(name,age,sex,color,temparament,alive) VALUES("Grinch",23,"M","Green","Angry",0);
+INSERT INTO bears(name,age,sex,color,temparament,alive) VALUES("Wendy",23,"F","Green","Angry",1);
+INSERT INTO bears(name,age,sex,color,temparament,alive) VALUES(null,23,"M","Green","Angry",0);
